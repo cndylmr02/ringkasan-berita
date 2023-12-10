574a21e949306fb246ec7e0505f5d60c639d5172
cindy <129759508+cndylmr02@users.noreply.github.com>
2023-12-11 05:48:49 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,61 +1,56 @@
 import streamlit as st
 import pandas as pd
-from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.model_selection import train_test_split
 from sklearn.pipeline import make_pipeline
+from sklearn.metrics.pairwise import cosine_similarity
+import networkx as nx
+import matplotlib.pyplot as plt
 
-# Download NLTK resources
-import nltk
-nltk.download('punkt')
-nltk.download('averaged_perceptron_tagger')
-nltk.download('stopwords')
-nltk.download('wordnet')
+# Load dataset
+df = pd.read_csv('data-uas.csv')  # Ganti dengan nama file dataset Anda
 
-def load_dataset():
-    # Load your dataset
-    # Modify the path accordingly
-    df = pd.read_csv('data-uas.csv')
-    return df
+# Train the model
+X_train, X_test, y_train, y_test = train_test_split(df['text'], df['category'], test_size=0.2, random_state=42)
+model = make_pipeline(TfidfVectorizer(), MultinomialNB())
+model.fit(X_train, y_train)
 
-def train_model(df):
-    # Split the dataset
-    X_train, X_test, y_train, y_test = train_test_split(df['Artikel'], df['Kategori'], test_size=0.2, random_state=42)
+# Calculate cosine similarity matrix
+tfidf_matrix = TfidfVectorizer().fit_transform(df['text'])
+cosine_sim = cosine_similarity(tfidf_matrix, tfidf_matrix)
 
-    # Create a pipeline with a TF-IDF vectorizer and a Naive Bayes classifier
-    model = make_pipeline(TfidfVectorizer(), MultinomialNB())
+# Create graph using cosine similarity
+G = nx.Graph()
+for i in range(len(df)):
+    for j in range(i+1, len(df)):
+        if cosine_sim[i, j] > 0.7:  # Adjust the threshold as needed
+            G.add_edge(df['text'][i], df['text'][j])
 
-    # Train the model
-    model.fit(X_train, y_train)
+# Calculate closeness centrality
+closeness_centrality = nx.closeness_centrality(G)
 
-    # Evaluate the model
-    accuracy = model.score(X_test, y_test)
-    st.write(f"Akurasi Model: {accuracy:.2f}")
+# Streamlit App
+st.title("Aplikasi untuk Mengklasifikasikan Kategori pada Suatu Berita")
 
-    return model
+# User input
+user_input = st.text_area("Masukkan Berita:", "")
 
-def predict_category(model, sentence):
-    # Predict the category for the input sentence
-    category = model.predict([sentence])[0]
-    return category
+# Predict category on button click
+if st.button("Prediksi Kategori"):
+    if user_input:
+        # Predict category
+        prediction = model.predict([user_input])[0]
+        st.write(f"Predicted Category: {prediction}")
 
-def main():
-    st.title("Aplikasi Klasisifikasi Kategori pada Kalimat")
-    st.write("Masukkan Kalimat berita untuk Memprediksi Kategorinya:")
+        # Display cosine similarity graph
+        st.subheader("Cosine Similarity Graph:")
+        pos = nx.spring_layout(G)
+        nx.draw(G, pos, with_labels=True, font_size=8, node_size=700, node_color='skyblue', font_color='black', font_weight='bold', edge_color='gray')
+        st.pyplot(plt)
 
-    # Load the dataset
-    df = load_dataset()
-
-    # Train the model
-    model = train_model(df)
-
-    # User input
-    sentence = st.text_input("Input Kalimat Berita:", "")
-
-    if st.button("Prediksi Kategori"):
-        # Predict and display the category
-        category = predict_category(model, sentence)
-        st.write(f"Hasil Prediksi: {category}")
-
-if __name__ == "__main__":
-    main()
+        # Display closeness centrality
+        st.subheader("Closeness Centrality:")
+        st.write(closeness_centrality)
+    else:
+        st.warning("Please enter a news headline.")
