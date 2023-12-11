12d9658abec179f41b4da8c4a354caaf60f1f348
cindy <129759508+cndylmr02@users.noreply.github.com>
2023-12-11 18:33:25 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 import streamlit as st
 import nltk
-from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.tokenize import sent_tokenize
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import ENGLISH_STOP_WORDS
 import networkx as nx
 import matplotlib.pyplot as plt
 
-# Download data untuk NLTK (dibutuhkan hanya pada instalasi pertama kali)
+# Download data untuk NLTK (hanya diperlukan pada instalasi pertama kali)
 nltk.download('punkt')
 
 # Title
-st.title('Aplikasi Ringkas Berita dengan Cosine Similarity dan Closeness Centrality')
+st.title('Aplikasi Ringkas Berita dengan Cosine Similarity, Closeness Centrality, dan Ekstraksi Kata Kunci')
 
 # Input Text Area
 berita_input = st.text_area("Masukkan teks berita:")
+
+# Jumlah kata kunci yang akan ditampilkan
+jumlah_kata_kunci = st.slider("Jumlah Kata Kunci yang Ditampilkan", min_value=1, max_value=10, value=5)
 
 # Button Summarize
 if st.button("Ringkas"):
@@ -23,7 +27,7 @@ if st.button("Ringkas"):
         kalimat = sent_tokenize(berita_input)
 
         # Vektorisasi TF-IDF
-        vectorizer = TfidfVectorizer()
+        vectorizer = TfidfVectorizer(stop_words='english')
         matriks_tfidf = vectorizer.fit_transform(kalimat)
 
         # Hitung cosine similarity
@@ -51,13 +55,26 @@ if st.button("Ringkas"):
 
         # Tampilkan closeness centrality
         st.subheader("Closeness Centrality:")
-        for i, centrality_score in closeness_centrality.items():
-            st.write(f"Kalimat {i+1}: {centrality_score:.4f}")
+        for node, centrality_score in closeness_centrality.items():
+            st.write(f"Kalimat {node+1}: {centrality_score:.4f}")
+
+        # Ekstraksi kata kunci berdasarkan TF-IDF
+        feature_names = vectorizer.get_feature_names_out()
+        tfidf_scores = matriks_tfidf.sum(axis=0).A1
+        top_keywords_indices = tfidf_scores.argsort()[-jumlah_kata_kunci:][::-1]
+        top_keywords = [feature_names[idx] for idx in top_keywords_indices]
 
         # Ringkasan berdasarkan kalimat yang memiliki cosine similarity tinggi dan closeness centrality rendah
-        top_sentences = [kalimat[i] for i in range(len(kalimat)) if closeness_centrality[i] < 0.5]
+        top_nodes = [node for node, score in closeness_centrality.items() if score < 0.5]
+        top_sentences = [kalimat[node] for node in top_nodes]
         summary = ' '.join(top_sentences)
 
-        # Tampilkan ringkasan
-        st.subheader("Ringkasan:")
-        st.write(summary)
+        # Tampilkan ringkasan, kata kunci, dan nilai kata kunci
+        if len(top_sentences) > 0:
+            st.subheader("Ringkasan:")
+            st.write(summary)
+            st.subheader(f"Top {jumlah_kata_kunci} Kata Kunci:")
+            for keyword, idx in zip(top_keywords, top_keywords_indices):
+                st.write(f"{keyword}: {tfidf_scores[idx]:.4f}")
+        else:
+            st.warning("Semua kalimat terkoneksi satu sama lain. Tidak ada yang dapat di-ringkas.")
