f76106c67efb7eeef1280cdd45af044389af8994
cindy <129759508+cndylmr02@users.noreply.github.com>
2023-12-11 12:02:23 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import matplotlib
+matplotlib.use("Agg")  # Tambahkan baris ini
+
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -11,59 +14,59 @@ from sklearn.metrics import confusion_matrix, accuracy_score, precision_recall_c
 from sklearn.preprocessing import MinMaxScaler
 
 # Title
-st.title('News Category Classification App')
+st.title('Aplikasi Klasifikasi Kategori Berita')
 
 # Load your news dataset
-# Make sure your dataset has 'text' for news content and 'category' for news category
-news_dataset = pd.read_csv(r'data-uas.csv')  # Replace 'your_news_dataset.csv' with your actual file path
+# Pastikan dataset Anda memiliki kolom 'text' untuk konten berita dan 'category' untuk kategori berita
+news_dataset = pd.read_csv('your_news_dataset.csv')  # Ganti 'your_news_dataset.csv' dengan jalur file aktual Anda
 
-# Display Dataset
-st.write('## News Dataset')
+# Tampilkan Dataset
+st.write('## Dataset Berita')
 st.dataframe(data=news_dataset)
 
 # Missing Value
-st.write('## Missing Value')
+st.write('## Nilai yang Hilang')
 st.write(news_dataset.isna().sum())
 
-# Features and Target
-features = news_dataset['text']
+# Fitur dan Target
+fitur = news_dataset['text']
 target = news_dataset['category']
 
-# TF-IDF Vectorization
-st.write("## TF-IDF Vectorization")
+# Vektorisasi TF-IDF
+st.write("## Vektorisasi TF-IDF")
 vectorizer = TfidfVectorizer(stop_words='english', max_features=5000)
-X = vectorizer.fit_transform(features).toarray()
+X = vectorizer.fit_transform(fitur).toarray()
 X = pd.DataFrame(X, columns=vectorizer.get_feature_names_out())
 
-# Display TF-IDF DataFrame
+# Tampilkan DataFrame TF-IDF
 st.dataframe(X)
 
-# Add Klasifikasi
+# Tambahkan Klasifikasi
 algorithm = st.sidebar.selectbox(
-    'Choose Algorithm',
+    'Pilih Algoritma',
     ('Multinomial Naive Bayes',)
 )
 
-# Add Parameter
-def add_parameters(algorithm):
+# Tambahkan Parameter
+def tambah_parameter(algorithm):
     params = dict()
     if algorithm == 'Multinomial Naive Bayes':
         alpha = st.sidebar.slider('Alpha', 0.1, 1.0, step=0.1)
         params['alpha'] = alpha
     return params
 
-parameters = add_parameters(algorithm)
+parameters = tambah_parameter(algorithm)
 
-# Choose Classification
-def choose_classification(algorithm, parameters):
+# Pilih Klasifikasi
+def pilih_klasifikasi(algorithm, parameters):
     classifier = None
     if algorithm == 'Multinomial Naive Bayes':
         classifier = MultinomialNB(alpha=parameters['alpha'])
     return classifier
 
-clf = choose_classification(algorithm, parameters)
+clf = pilih_klasifikasi(algorithm, parameters)
 
-# Classification Process
+# Proses Klasifikasi
 X_train, X_test, y_train, y_test = train_test_split(X, target, test_size=0.2, random_state=1234)
 
 clf.fit(X_train, y_train)
@@ -72,45 +75,45 @@ acc = accuracy_score(y_test, pred_labels)
 
 col1, col2 = st.columns(2)
 with col1:
-    st.write(f'#### Algorithm = {algorithm}')
+    st.write(f'#### Algoritma = {algorithm}')
 with col2:
-    st.write('#### Accuracy = {}%'.format(round(acc*100)))
+    st.write('#### Akurasi = {}%'.format(round(acc*100)))
 
 precision, recall, threshold = precision_recall_curve(y_test, pred_labels)
 
 with col1:
-    st.write(f"Precision  =  {precision[0]}")
+    st.write(f"Presisi  =  {precision[0]}")
 with col2:
     st.write(f"Recall  =  {recall[0]}")
 
-# Performance Metrics Graph
-st.write("## Performance Metrics Graph")
-plt.plot(threshold, precision[:-1], label='Precision')
+# Grafik Metrik Kinerja
+st.write("## Grafik Metrik Kinerja")
+plt.plot(threshold, precision[:-1], label='Presisi')
 plt.plot(threshold, recall[:-1], label='Recall')
-plt.plot(acc*np.ones_like(threshold), label='Accuracy')
+plt.plot(acc*np.ones_like(threshold), label='Akurasi')
 plt.xlabel('Threshold')
-plt.ylabel('Score')
-plt.title('Performance Metrics')
+plt.ylabel('Skor')
+plt.title('Metrik Kinerja')
 plt.legend()
 st.pyplot(plt.show())
 
-# Confusion Matrix
-st.write("## Confusion Matrix")
+# Matriks Confusion
+st.write("## Matriks Confusion")
 f, ax = plt.subplots(figsize=(8, 5))
 sns.heatmap(confusion_matrix(y_test, pred_labels), annot=True, fmt=".0f", ax=ax)
-plt.xlabel("Predicted Category")
-plt.ylabel("True Category")
+plt.xlabel("Kategori Terprediksi")
+plt.ylabel("Kategori Sebenarnya")
 st.pyplot(plt.show())
 
-# Prediction for New Data
-st.write('# Predict News Category for New Data')
+# Prediksi untuk Data Baru
+st.write('# Prediksi Kategori Berita untuk Data Baru')
 
-news_input = st.text_area("Enter the news content:")
+berita_input = st.text_area("Masukkan konten berita:")
 
-# Button Predict
-prediction = ''
-if st.button("Predict"):
-    process_data = vectorizer.transform([news_input]).toarray()
-    predict_category = clf.predict(process_data)
-    prediction = predict_category[0]
-    st.success(f'Predicted Category: {prediction}')
+# Tombol Prediksi
+prediksi = ''
+if st.button("Prediksi"):
+    data_proses = vectorizer.transform([berita_input]).toarray()
+    prediksi_kategori = clf.predict(data_proses)
+    prediksi = prediksi_kategori[0]
+    st.success(f'Kategori Terprediksi: {prediksi}')
